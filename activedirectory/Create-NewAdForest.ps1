7733aba8bb89fb3a109fb4467dc7e560bbdb9950
jesse-wr <44740145+jesse-wr@users.noreply.github.com>
2020-02-20 19:30:41 +0930

    added initial dns config and win feature installs

--- activedirectory/Create-NewAdForest.ps1
+++ activedirectory/Create-NewAdForest.ps1
@@ -1,17 +1,34 @@
-#
-# Export of ADDS Deployment from GUI
-#
+Install-WindowsFeature -Name ad-domain-services,RSAT-AD-Tools,RSAT-AD-PowerShell,RSAT-ADDS, DNS, RSAT-DNS-Server
+
+$smp = ConvertTo-SecureString -String "<PASSWORD>" -AsPlainText -Force
+
 
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
 -DomainMode "WinThreshold" `
--DomainName "ad.contoso.org.au" `
--DomainNetbiosName "AD" `
+-DomainName "lab.local" `
+-DomainNetbiosName "lab" `
 -ForestMode "WinThreshold" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
+-SafeModeAdministratorPassword $smp `
 -Force:$true
+
+#DNS
+Import-Module DnsServer
+
+#Set dns forwarded
+Set-DnsServerForwarder -IPAddress 1.1.1.1 -ComputerName "<NEW DC HOSTNAME>"
+
+#Set dns listen interfaces
+dnscmd "<NEW DC HOSTNAME>" /ResetListenAddresses "<NEW DC IP>"
+
+#Create reverse zone
+Add-DnsServerPrimaryZone -NetworkID "<0.0.0.0/0>" -ReplicationScope "Forest"
+
+#Add PTR for server
+Add-DnsServerResourceRecordPtr -Name 10 -PtrDomainName "<NEW DC FQDN>" -ZoneName "<0.0.0>.in-addr.arpa" -AgeRecord -AllowUpdateAny
