44cd9dcacf2ec6abf9545021281418a256bc963d
Jesse Rutledge Admin <admjrutl@ntschools.net>
2020-10-16 09:46:47 +0930

    mofified ntfs script

--- windows_server/Audit-NTFSPermissions.ps1
+++ windows_server/Audit-NTFSPermissions.ps1
@@ -1,13 +1,16 @@
-$FolderPath = Get-ChildItem -Directory -Path "\\<server>\<share>" -Recurse -Force
+$FolderPath = Get-ChildItem -Directory -Path "\\<server>\<share>" -Force
 $Report = @()
 Foreach ($Folder in $FolderPath) {
     $Acl = Get-Acl -Path $Folder.FullName
     foreach ($Access in $acl.Access)
         {
-            $Properties = [ordered]@{'FolderName'=$Folder.FullName;'AD
-Group or
-User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+            $Properties = [ordered]@{
+                'FolderName' = $Folder.FullName;
+                'ADGroup or User' = $Access.IdentityReference;
+                'Permissions' = $Access.FileSystemRights;
+                'Inherited' = $Access.IsInherited
+                 }
             $Report += New-Object -TypeName PSObject -Property $Properties
         }
 }
-$Report | Export-Csv -path "C:\FolderPermissions.csv"
+$Report | Export-Csv -path "C:\Temp\FolderPermissions.csv" -NoTypeInformation -NoClobber
